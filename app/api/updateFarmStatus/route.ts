18af119caa645beef50ff56abb1506bd661f19b8
Endolf <agustinrubenfernandez@gmail.com>
2025-03-23 06:47:24 -0300

    add logs

--- app/api/updateFarmStatus/route.ts
+++ app/api/updateFarmStatus/route.ts
@@ -7,7 +7,11 @@ export async function PUT(req: NextRequest) {
     const body = await req.json();
     const { args } = body;
     const provider = new ethers.JsonRpcProvider(process.env.RPC_PROVIDER);
+    console.log("process.env.RPC_PROVIDER)", process.env.RPC_PROVIDER);
+    console.log("provider", provider);
     const wallet = new ethers.Wallet("0x" + process.env.PRIVATE_KEY, provider);
+    console.log("process.env.PRIVATE_KEY", process.env.PRIVATE_KEY);
+    console.log("wallet", wallet);
     const integrationContract = new ethers.Contract(
       CONTRACT_ADDRESS,
       INTEGRATION_ABI,
